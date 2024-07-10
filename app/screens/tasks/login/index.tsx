08c0069b71945a89d361f6665b26d7b807c5bc66
wanyike <wanyikeemmanuel@gmail.com>
2024-07-11 00:34:22 +0300

    editing

--- app/screens/tasks/login/index.tsx
+++ app/screens/tasks/login/index.tsx
@@ -13,8 +13,9 @@ export default function login() {
       <Stack.Screen
         options={{ title: "settings", headerTitleAlign: "center" }}
       />
-      <View>
+      <View className="flex-row">
         <View>
+          <Camera color="black" size={48} />
           <Text>icon</Text>
         </View>
         <View>
@@ -23,6 +24,67 @@ export default function login() {
           </Text>
         </View>
         <View>
+          <Text>This allows us to send you promos, deals among things</Text>
+        </View>
+        <View>
+          <Camera color="black" size={48} />
+          <Text>sweetch</Text>
+        </View>
+      </View>
+
+      <View className="flex-row">
+        <View>
+          <Camera color="black" size={48} />
+          <Text>Rock</Text>
+        </View>
+        <View>
+          <Text className="font-normal text-base[17px] leading-5 tracking-tighter[0-43em]">
+            Privacy
+          </Text>
+        </View>
+        <View>
+          <Text>Manage the data you share with us</Text>
+        </View>
+        <View>
+          <Camera color="black" size={48} />
+          <Text>Arrow</Text>
+        </View>
+      </View>
+
+      <View className="flex-row">
+        <View>
+          <Camera color="black" size={48} />
+          <Text>icon</Text>
+        </View>
+        <View>
+          <Text className="font-normal text-base[17px] leading-5 tracking-tighter[0-43em]">
+            Appearance
+          </Text>
+        </View>
+        <View>
+          <Text>Select how you want your app to look like</Text>
+        </View>
+        <View>
+          <Camera color="black" size={48} />
+          <Text>sweetch</Text>
+        </View>
+      </View>
+
+      <View className="flex-row">
+        <View>
+          <Camera color="black" size={48} />
+          <Text>call</Text>
+        </View>
+        <View>
+          <Text className="font-normal text-base[17px] leading-5 tracking-tighter[0-43em]">
+            Communication
+          </Text>
+        </View>
+        <View>
+          <Text>Choose your preferred contact methods</Text>
+        </View>
+        <View>
+          <Camera color="black" size={48} />
           <Text>sweetch</Text>
         </View>
       </View>
