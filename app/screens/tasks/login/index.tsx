8bc9b2d553f33a20496c5d39d9e079f4558b63fd
wanyike <wanyikeemmanuel@gmail.com>
2024-07-10 22:57:01 +0300

    change

--- app/screens/tasks/login/index.tsx
+++ app/screens/tasks/login/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Link, router, Stack, Tabs } from "expo-router";
 import { ArrowLeft } from "lucide-react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Camera } from "lucide-react-native";
 
 export default function login() {
   function handlelogih(text: string): void {}
@@ -12,27 +13,18 @@ export default function login() {
       <Stack.Screen
         options={{ title: "settings", headerTitleAlign: "center" }}
       />
-      <Text className="">Notifications</Text>
-      <Text className="">
-        This allows us to send you proms, deals among things
-      </Text>
-      <View className="">
-        <Pressable className="">
-          <Text className="">Privacy</Text>
-          <Text className="">Manager the data you share with us</Text>
-        </Pressable>
-        <Text className="">Appearance</Text>
-        <Text className="">Select how you want your app to look like</Text>
-        <Pressable className="">
-          <Text className=""></Text>
-        </Pressable>
-        <Pressable className="">
-          <Text className="">Communication</Text>
-          <Text className="">Choose your preferred contact methods</Text>
-        </Pressable>
-        <Pressable className="">
-          <Text className="">Next</Text>
-        </Pressable>
+      <View>
+        <View>
+          <Text>icon</Text>
+        </View>
+        <View>
+          <Text className="font-normal text-base[17px] leading-5 tracking-tighter[0-43em]">
+            Notifications
+          </Text>
+        </View>
+        <View>
+          <Text>sweetch</Text>
+        </View>
       </View>
     </View>
   );
