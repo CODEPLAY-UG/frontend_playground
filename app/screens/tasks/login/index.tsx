443dfbc38d364d3c860255a7966daf3d73bede3d
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-07-12 17:21:58 +0300

    style: Improved the layout
    Also tested the functionality of react native paper

--- app/screens/tasks/login/index.tsx
+++ app/screens/tasks/login/index.tsx
@@ -10,7 +10,8 @@ import {
   Phone,
   SunMoon,
 } from "lucide-react-native";
-import { Switch } from "@fluentui/react-components";
+import { Button } from "react-native-paper";
+// import { Switch } from "@fluentui/react-components";
 
 export default function login() {
   function handlelogih(text: string): void {}
@@ -20,98 +21,88 @@ export default function login() {
       <Stack.Screen
         options={{ title: "Settings", headerTitleAlign: "center" }}
       />
-      <View className=" gap-y-5 mt-[12px]">
+      <View className=" space-y-5 mt-[12px]">
         <View className="flex-row justify-between items-center">
-          <View className="">
+          <View className="flex-row items-center">
             <BellIcon color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Notifications
+              </Text>
+              <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                This allows us to send you promos, {"\n"} deals among things
+              </Text>
+            </View>
           </View>
-          <View className="">
-            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Notifications
-            </Text>
-            <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              This allows us to send you promos, {"\n"} deals among things
-            </Text>
-          </View>
+
           <View className="">
             {/* <Switch /> */}
             <BellIcon color="#808080" size={20} />
           </View>
         </View>
         <View className="flex-row justify-between items-center">
-          <View className="">
+          <View className="flex-row items-center">
             <Lock color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className=" text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Privacy
+              </Text>
+              <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                Manage the data you share with us
+              </Text>
+            </View>
           </View>
-          <View className="">
-            <Text className=" text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Privacy
-            </Text>
-            <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              Manage the data you share with us
-            </Text>
-          </View>
+
           <View className="">
             <ChevronRight color="#808080" size={20} />
           </View>
         </View>
         <View className="flex-row justify-between items-center  ">
-          <View className="">
+          <View className="flex-row items-center">
             <SunMoon color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Appearance
+              </Text>
+              <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                Select how you want your app to look like
+              </Text>
+            </View>
           </View>
-          <View className="">
-            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Appearance
-            </Text>
-            <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              Select how you want your app to look like
-            </Text>
-          </View>
+
           <View className="">
             <ChevronRight color="#808080" size={20} />
           </View>
         </View>
         <View className="flex-row justify-between items-center  ">
-          <View className="">
+          <View className="flex-row items-center">
             <Phone color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className="text-[#242424] h-[22px] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Communication
+              </Text>
+              <Text className=" text-[#616161] h-[18px] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                Choose your preferred contact methods
+              </Text>
+            </View>
           </View>
-          <View className="">
-            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Communication
-            </Text>
-            <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              Choose your preferred contact methods
-            </Text>
-          </View>
+
           <View className="">
             <ChevronRight color="#808080" size={20} />
           </View>
         </View>
       </View>
 
-      <View className="flex-row justify-between items-center mt-auto">
-        <View className="mt-4 justify-center items-center space-y-1">
-          <Image
-            className="w-[22.5px] h-[21px]"
-            source={require("@assets/images/home.png")}
-          />
-          <Text>Home</Text>
-        </View>
-
-        <View className="mt-4 justify-center items-center space-y-1">
-          <Image
-            className="w-[22.5px] h-[21px]"
-            source={require("@assets/images/activity.png")}
-          />
-          <Text>Activity</Text>
-        </View>
-
-        <View className="mt-4 justify-center items-center space-y-1">
-          <Image
-            className="w-[22.5px] h-[21px]"
-            source={require("@assets/images/profile.png")}
-          />
-          <Text>Profile</Text>
-        </View>
+      <View className="items-center justify-center">
+        <Button
+          rippleColor="#FF000020"
+          className=""
+          icon="camera"
+          mode="contained"
+          onPress={() => {}}
+        >
+          Press me
+        </Button>
       </View>
     </View>
   );
