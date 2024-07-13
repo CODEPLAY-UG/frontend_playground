6722254ced24900f6ceb89aa3994b2d736697bfe
wanyike <wanyikeemmanuel@gmail.com>
2024-07-13 15:08:29 +0300

    chore: update two screens
    update setting and login & security screens

--- app/screens/tasks/Emma/index.tsx
+++ app/screens/tasks/Emma/index.tsx
@@ -18,83 +18,93 @@ import {
   Phone,
   SunMoon,
 } from "lucide-react-native";
-
+import { Button } from "react-native-paper";
+import { Switch } from "react-native-paper";
 
 export default function settings() {
   function handleSettings(text: string): void {}
-  
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
   return (
     <View className="bg-white h-full w-full px-5">
       <Stack.Screen
         options={{ title: "Settings", headerTitleAlign: "center" }}
       />
-      <View className=" space-y-5 mt-[12px]">
+      <View className="space-y-5 mt-[12px]">
         <View className="flex-row justify-between items-center">
           <View className="flex-row items-center">
             <BellIcon color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Notifications
+              </Text>
+              <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                This allows us to send you promos, {"\n"} deals among things
+              </Text>
+            </View>
           </View>
-          <View className="mx-4">
-            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Notifications
-            </Text>
-            <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              This allows us to send you promos, {"\n"} deals among things
-            </Text>
-          </View>
-          <View className="flex-row items-center">
+
+          <View className="">
             {/* <Switch /> */}
-            <BellIcon color="#808080" size={20} />
+            <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
           </View>
         </View>
         <View className="flex-row justify-between items-center">
           <View className="flex-row items-center">
             <Lock color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className=" text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Privacy
+              </Text>
+              <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                Manage the data you share with us
+              </Text>
+            </View>
           </View>
-          <View className="mx-4">
-            <Text className=" text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Privacy
-            </Text>
-            <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              Manage the data you share with us
-            </Text>
-          </View>
+
           <View className="">
             <ChevronRight color="#808080" size={20} />
           </View>
         </View>
-        <View className="flex-row justify-between items-center  ">
+
+        <View className="flex-row justify-between items-center ">
           <View className="flex-row items-center">
             <SunMoon color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Appearance
+              </Text>
+              <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                Select how you want your app to look like
+              </Text>
+            </View>
           </View>
-          <View className="mx-4">
-            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Appearance
-            </Text>
-            <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              Select how you want your app to look like
-            </Text>
-          </View>
+
           <View className="">
             <ChevronRight color="#808080" size={20} />
           </View>
         </View>
+
         <View className="flex-row justify-between items-center  ">
           <View className="flex-row items-center">
             <Phone color="#808080" size={20} />
+            <View className="mx-4">
+              <Text className="text-[#242424] h-[22px] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+                Communication
+              </Text>
+              <Text className=" text-[#616161] h-[18px] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+                Choose your preferred contact methods
+              </Text>
+            </View>
           </View>
-          <View className="mx-4">
-            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
-              Communication
-            </Text>
-            <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
-              Choose your preferred contact methods
-            </Text>
-          </View>
+
           <View className="">
             <ChevronRight color="#808080" size={20} />
           </View>
         </View>
+        
       </View>
 
       <View className="justify-center gap-5 items-center mt-40 mb-2">
@@ -103,32 +113,6 @@ export default function settings() {
             <Text className="text-[#426380] text-lg">Next Page</Text>
           </Pressable>
         </Link>
-      </View>
-
-      <View className="flex-row justify-between items-center mt-auto">
-        <View className="mt-4 justify-center items-center space-y-1">
-          <Image
-            className="w-[22.5px] h-[21px]"
-            source={require("@assets/images/home.png")}
-          />
-          <Text>Home</Text>
-        </View>
-
-        <View className="mt-4 justify-center items-center space-y-1">
-          <Image
-            className="w-[22.5px] h-[21px]"
-            source={require("@assets/images/activity.png")}
-          />
-          <Text>Activity</Text>
-        </View>
-
-        <View className="mt-4 justify-center items-center space-y-1">
-          <Image
-            className="w-[22.5px] h-[21px]"
-            source={require("@assets/images/profile.png")}
-          />
-          <Text>Profile</Text>
-        </View>
       </View>
     </View>
   );
--- app/screens/tasks/Emma/login&security.tsx
+++ app/screens/tasks/Emma/login&security.tsx
@@ -1,10 +1,78 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Pressable, Image } from "react-native";
+import React, { useState } from "react";
+import { Link, router, Stack, Tabs } from "expo-router";
+import { Button } from "react-native-paper";
+import { Switch } from "react-native-paper";
 
 export default function Acounts() {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+
   return (
-    <View>
-      <Text>Acounts</Text>
+    <View className="bg-white h-full w-full px-5">
+      <Stack.Screen
+        options={{ title: "Login & security", headerTitleAlign: "center" }}
+      />
+
+      <View className=" space-y-5 mt-[12px]">
+        <View className="flex-row justify-between items-center">
+          <View className="mx-4">
+            <Text className="text-[#242424] text-[17px] font-semibold leading-[22px] tracking-tighter[-0.43]">
+              Connected accounts
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View className="flex-row justify-between items-center">
+        <View className="flex-row items-center">
+          <Image
+            className="h-10 w-10"
+            source={require(".../../../assets/images/googleLogo.png")}
+          />
+          <View className="mx-4">
+            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+              Google
+            </Text>
+            <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+              example@gmail.com
+            </Text>
+          </View>
+        </View>
+
+        <View className="">
+          <Switch
+            className={"w-[31px] h-[51px] rounded-[20px]"}
+            value={isSwitchOn}
+            onValueChange={onToggleSwitch}
+          />
+        </View>
+      </View>
+
+      <View className="flex-row justify-between items-center">
+        <View className="flex-row items-center">
+          <Image
+            className="h-10 w-10"
+            source={require(".../../../assets/images/appleLogo.png")}
+          />
+          <View className="mx-4">
+            <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">
+              Apple
+            </Text>
+            <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">
+              example@gmail.com
+            </Text>
+          </View>
+        </View>
+
+        <View className="">
+          <Switch
+            className={"w-[31px] h-[51px] rounded-[20px]"}
+            onValueChange={onToggleSwitch}
+          />
+        </View>
+      </View>
     </View>
-  )
+  );
 }
