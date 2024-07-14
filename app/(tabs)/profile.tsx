e5b4397cca0b5313cfa7bd02abff161826bf9c1f
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-07-14 21:32:55 +0300

    style: fixed some styling issues
    Removed fixed width from some elements and also  refactored some flexbox layout designs.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,96 +2,101 @@ import { View, Text, Image, Pressable } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack, router } from "expo-router";
+import { ArrowLeft, ArrowRight, ChevronRight } from "lucide-react-native";
 
 export default function Profile() {
   return (
-    <SafeAreaView className=" bg-white h-full w-full px-6 flex-1">
-      {/* i wasnt able to give it a stack.screen with the profile header so im using mt-70px till im able to do it*/}
+    <SafeAreaView className=" bg-white h-full w-full">
       <Stack.Screen
         options={{
           headerShown: true,
           headerTitle: "Profile",
         }}
       />
-      <View className=" flex h-[136px] gap-x-[2px]">
-        <View className="justify-center items-center">
-          <Image source={require("../../assets/images/avatar.png")} />
-        </View>
-        <Pressable>
-          <View className="flex flex-row  items-center p-4 gap-x-[210px]">
-            <View>
-              <Text className="text-[17px] leading-[22px] text-[#242424] font-[400]">
-                Name
-              </Text>
-              <Text className="text-[13px] leading-[18px] text-[#616161] font-[400]">
-                Phone number/email
+      <View className="mx-6">
+          <View className="justify-center items-center">
+            <Image source={require("../../assets/images/avatar.png")} />
+          </View>
+          <Pressable onPress={() => router.push("../screens/profile/personal_Info")}>
+            <View className="flex-row justify-between  items-center">
+              <View className="py-3">
+                <Text className="text-[17px] leading-[22px] text-[#242424] font-[400]">
+                  Name
+                </Text>
+                <Text className="text-[13px] leading-[18px] text-[#616161] font-[400]">
+                  Phone number/email
+                </Text>
+              </View>
+              {/* <Image source={require("../../assets/images/chevronRight.png")} /> */}
+              <ChevronRight color="#808080" size={16} />
+            </View>
+          </Pressable>
+          <View className="gap-y-6 h-[336px] mt-1 ">
+            <View className="flex-row justify-between">
+              <View className="flex-row gap-x-3">
+                <Image source={require("../../assets/images/settings.png")} />
+                <Text className="text-[17px] leading-[22px] text-[#242424] font-[400] ">
+                  Settings
+                </Text>
+              </View>
+              <ChevronRight color="#808080" size={16} />
+          </View>
+          <View className="">
+            <View className="flex-row gap-x-3 justify-between">
+              <View className="flex-row space-x-3">
+                <Image source={require("../../assets/images/security.png")} />
+                <Text className="text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
+                  Login & security
+                </Text>
+              </View>
+              <ChevronRight color="#808080" size={16} />
+            </View>
+          </View>
+          <View className="">
+            <View className="flex flex-row gap-x-3 justify-between">
+              <View className="flex-row space-x-3">
+                <Image source={require("../../assets/images/help.png")} />
+                <Text className="text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
+                  Help
+                </Text>
+              </View>
+              <ChevronRight color="#808080" size={16} />
+            </View>
+
+          </View>
+          <View className="flex flex-row gap-x-[2px]">
+            <View className="flex flex-row gap-x-[12px]">
+              <Image source={require("../../assets/images/feedback.png")} />
+              <Text className="w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
+                Give us feedback
               </Text>
             </View>
-            <Image source={require("../../assets/images/chevronRight.png")} />
+            <ChevronRight color="#808080" size={16} />
           </View>
-        </Pressable>
-      </View>
-
-      <View className="flex flex-col gap-y-[24px] h-[336px] mt-[20px] ">
-        <View className="flex flex-row gap-x-[2px]  ">
-          <View className="flex flex-row gap-x-[12px]  ">
-            <Image source={require("../../assets/images/settings.png")} />
-            <Text className=" w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
-              Settings
+          <View className="flex flex-row gap-x-[2px] ">
+            <View className="flex flex-row gap-x-[12px]">
+              <Image source={require("../../assets/images/legal.png")} />
+              <Text className="w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
+                Legal
+              </Text>
+            </View>
+            <ChevronRight color="#808080" size={16} />
+          </View>
+          <View className="flex flex-row gap-x-[2px] ">
+            <View className="flex flex-row gap-x-[12px]">
+              <Image source={require("../../assets/images/info.png")} />
+              <Text className="w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px]">
+                About
+              </Text>
+            </View>
+            <ChevronRight color="#808080" size={16} />
+          </View>
+          <Pressable className="items-center justify-center ">
+            <Text className="  text-[#C50F1F] font-[400]  text-[17px] leading-[22px]">
+              Log out
             </Text>
-          </View>
-          <Image source={require("../../assets/images/chevronRight.png")} />
+          </Pressable>
         </View>
-        <View className="flex flex-row gap-x-[2px] ">
-          <View className="flex flex-row gap-x-[12px]">
-            <Image source={require("../../assets/images/security.png")} />
-            <Text className="w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
-              Login & security
-            </Text>
-          </View>
-          <Image source={require("../../assets/images/chevronRight.png")} />
-        </View>
-        <View className="flex flex-row gap-x-[2px] ">
-          <View className="flex flex-row gap-x-[12px]">
-            <Image source={require("../../assets/images/help.png")} />
-            <Text className="w-[280px]  text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
-              Help
-            </Text>
-          </View>
-          <Image source={require("../../assets/images/chevronRight.png")} />
-        </View>
-        <View className="flex flex-row gap-x-[2px]">
-          <View className="flex flex-row gap-x-[12px]">
-            <Image source={require("../../assets/images/feedback.png")} />
-            <Text className="w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
-              Give us feedback
-            </Text>
-          </View>
-          <Image source={require("../../assets/images/chevronRight.png")} />
-        </View>
-        <View className="flex flex-row gap-x-[2px] ">
-          <View className="flex flex-row gap-x-[12px]">
-            <Image source={require("../../assets/images/legal.png")} />
-            <Text className="w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px] ">
-              Legal
-            </Text>
-          </View>
-          <Image source={require("../../assets/images/chevronRight.png")} />
-        </View>
-        <View className="flex flex-row gap-x-[2px] ">
-          <View className="flex flex-row gap-x-[12px]">
-            <Image source={require("../../assets/images/info.png")} />
-            <Text className="w-[280px] text-[17px] leading-[22px] text-[#242424] font-[400] p-[1px]">
-              About
-            </Text>
-          </View>
-          <Image source={require("../../assets/images/chevronRight.png")} />
-        </View>
-        <Pressable className="items-center justify-center ">
-          <Text className="  text-[#C50F1F] font-[400]  text-[17px] leading-[22px]">
-            Log out
-          </Text>
-        </Pressable>
       </View>
     </SafeAreaView>
   );
